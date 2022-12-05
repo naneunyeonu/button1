50f30e4e1e07055decf24b04cf053e4a088caacd
Yeon Woo <evan2011@naver.com>
2022-12-05 10:34:19 +0900

    change name

--- button1.py
+++ button1.py
@@ -1,9 +1,9 @@
 from tkinter import *
 
-window =Tk()
+window = Tk()
 
-b1 = Button(window, text="Yeonseung Ryu")
-b2 = Button(window, text="60161111")
+b1 = Button(window, text="YeonWoo Chae")
+b2 = Button(window, text="60212578")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
 
